405dc3a0b5a7d863a37cef51b5db585f70866034
Toby HUANG <z5141448@ad.unsw.edu.au>
2019-06-16 16:52:18 +1000

    ellipse experiement is finished

---
+++ lec/lec02/Ellipses/Ellipses.hs
@@ -0,0 +1,28 @@
+module Ellipses where
+
+-- needed to display the picture in the playground
+import Codec.Picture
+
+-- our line graphics programming interface
+import ShapeGraphics
+    
+simpleEllipsePic :: Float ->Picture
+simpleEllipsePic n
+        = map greenEllipse [0, pi/n .. (n-1) * pi /n]
+    where 
+        centre :: Point 
+        centre  = Point 400 400
+        greenEllipse :: Float -> PictureObject
+        greenEllipse angle 
+            = Ellipse centre 250 70 angle (colourFor angle) Solid SolidFill
+        -- myGreen :: Colour 
+        -- myGreen  = Colour 27 230 34 80
+
+        colourFor angle 
+            = let x = round (255 * angle/ pi)
+                in Colour (255 - x ) 128 x 84
+
+
+writeToFile pic
+    = writePng "output.png" 
+        (drawPicture 3 pic)
