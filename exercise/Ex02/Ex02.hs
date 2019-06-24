18031d5a93a53dfe8c23cf63757302355146390a
Toby HUANG <z5141448@ad.unsw.edu.au>
2019-06-24 16:36:22 +1000

    finish the definition of dodgy sort

---
+++ exercise/Ex02/Ex02.hs
@@ -0,0 +1,63 @@
+module Ex02 where
+import Test.QuickCheck
+import Data.List
+-- implement the following functions, which meet some (but not all!) of the 
+-- properties of a correct sorting function
+
+-- prop2 & 4, but not prop1 & 3 & 5
+dodgySort1 :: [Int] -> [Int]
+dodgySort1 xs = xs
+
+
+-- prop1 & 2 & 3, but not prop4 & 5
+dodgySort2 :: [Int] -> [Int]
+dodgySort2 xs 
+  | xs == []  = [] 
+  | otherwise = [minimum(xs)..maximum(xs)]
+
+
+-- prop1 & 3 & 4, but not prop2 & 5
+dodgySort3 :: [Int] -> [Int]
+dodgySort3 xs = cycle [1]
+
+
+-- prop1 & 2 & 3 & 4, but not prop5
+dodgySort4 :: [Int] -> [Int]
+dodgySort4 xs =  (take (length(xs) - length(uniqueSorted)) $ cycle [head uniqueSorted]) ++ uniqueSorted
+  where 
+    uniqueSorted = nub $ sort  xs
+  
+-- Properties of sorting function    
+
+-- sort reverse is same as sorted 
+sortProp1 :: ([Int] -> [Int]) -> [Int] -> Bool
+sortProp1 sortFn xs = sortFn xs == sortFn (reverse xs)
+
+-- all integer must be inside the arrays
+sortProp2 :: ([Int] -> [Int]) -> Int -> [Int] -> [Int] -> Bool
+sortProp2 sortFn x xs ys = x `elem` sortFn (xs ++ [x] ++ ys)
+
+-- all the element is sorted 
+sortProp3 :: ([Int] -> [Int]) -> [Int] -> Bool
+sortProp3 sortFn xs = isSorted (sortFn xs)
+  where 
+    isSorted (x1 : x2 : xs) = (x1 <= x2) && isSorted (x2 : xs)
+    isSorted _ = True
+    
+-- two array has same length
+sortProp4 :: ([Int] -> [Int]) -> [Int] -> Bool    
+sortProp4 sortFn xs = length xs == length (sortFn xs)
+
+-- good sort
+sortProp5 :: ([Int] -> [Int]) -> [Int] -> Bool
+sortProp5 sortFn xs 
+  = sortFn xs == insertionSort xs
+
+insertionSort :: [Int] -> [Int]
+insertionSort xs = foldr insertSorted [] xs
+  where 
+    insertSorted x [] = [x]
+    insertSorted x (y : ys) 
+      | x <= y = x : y : ys
+      | otherwise = y : insertSorted x ys
+
