1f3a2dc0f4b4a9f7278b2d8fed6a064aa71637b1
Toby HUANG <z5141448@ad.unsw.edu.au>
2019-07-12 16:34:31 +1000

    import this weeks' exercise files

---
+++ exercise/Ex04/Ex04.hs
@@ -0,0 +1,56 @@
+module Ex04 where
+
+import Text.Read (readMaybe)
+
+data Token = Number Int | Operator (Int -> Int -> Int)
+
+parseToken :: String -> Maybe Token
+parseToken "+" = Just (Operator (+))
+parseToken "-" = Just (Operator (-))
+parseToken "/" = Just (Operator div)
+parseToken "*" = Just (Operator (*))
+parseToken str = fmap Number (readMaybe str)
+
+tokenise :: String -> Maybe [Token]
+tokenise = error "'tokenise' unimplemented"
+
+
+newtype Calc a = C ([Int] -> Maybe ([Int], a))
+
+
+pop :: Calc Int
+pop = error "'pop' unimplemented"
+
+push :: Int -> Calc ()
+push i = error "'push' unimplemented"
+
+
+instance Functor Calc where
+  fmap f (C sa) = C $ \s ->
+      case sa s of 
+        Nothing      -> Nothing
+        Just (s', a) -> Just (s', f a)
+
+instance Applicative Calc where
+  pure x = C (\s -> Just (s,x))
+  C sf <*> C sx = C $ \s -> 
+      case sf s of 
+          Nothing     -> Nothing
+          Just (s',f) -> case sx s' of
+              Nothing      -> Nothing
+              Just (s'',x) -> Just (s'', f x)
+
+instance Monad Calc where
+  return = pure
+  C sa >>= f = C $ \s -> 
+      case sa s of 
+          Nothing     -> Nothing
+          Just (s',a) -> unwrapCalc (f a) s'
+    where unwrapCalc (C a) = a
+
+evaluate :: [Token] -> Calc Int
+evaluate ts = error "'evaluate' unimplemented"
+
+calculate :: String -> Maybe Int
+calculate s = error "'calculate' unimplemented"
+
